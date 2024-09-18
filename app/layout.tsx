1389df441c50cdf1ef8c0e2e3bdde7e5df1f4c75
Alan <soldier4443@gmail.com>
2024-09-19 07:04:34 +0900

    add custom fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
+import { inter } from '@/app/ui/fonts';
 import '@/app/ui/global.css';
+import clsx from 'clsx';
 
 export default function RootLayout({
   children,
@@ -7,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={clsx(inter.className, 'antialiased')}>{children}</body>
     </html>
   );
 }
